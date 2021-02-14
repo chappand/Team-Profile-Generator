a7b95428b7acaf986d5e99c9e82ee51a93e23714
Andrew Chappell <chappand@gmail.com>
2021-02-13 22:57:38 -0500

    Added tests to Employee testjs.

--- Tests/Employee.test.js
+++ Tests/Employee.test.js
@@ -1,12 +1,25 @@
-const { it } = require("@jest/globals");
-const Employee = require("Lib/Employee.js");
+const Employee = require('../Lib/Employee');
 
 describe("Employee", () => {
     describe("Initialization", () => {
         it("Should create a new object with Name, Id, and Email when called with the 'new' keyword", () => {
-            const obj = new Employee();
+            const obj = new Employee("Andrew", 1111, "x@x.com");
+            function getRole() {
+                return "Employee";
+            };
 
-            
+            expect(Employee.name !== null).toEqual(true);
+            expect(Employee.id !== null).toEqual(true);
+            expect(Employee.email !== null).toEqual(true);
+            expect(getRole()).toEqual("Employee");
+        });
+    });
+
+    describe("name", () => {
+        it("New Employee should have the same name that is passed in", () => {
+            const emp = new Employee("Andrew", 1111, "x@x.com");
+
+            expect(Employee.name).toEqual("Andrew");
         });
     });
 });
