23af97a664e5c3a0272345fdaafde0024effc7de
Andrew Chappell <chappand@gmail.com>
2021-02-13 20:59:53 -0500

    Employee Class built

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquire = require("inquirer");
+const Employee = require("Lib/Employee.js");
+const Engineer = require("Lib/Engineer.js");
+const Intern = require("Lib/Intern.js");
+const Manager = require("Lib/Manager.js");
+
+
+
