51156e359ede771046243477fa40d03a9d74d3c3
Andrew Chappell <chappand@gmail.com>
2021-02-14 00:33:31 -0500

    Engineer Test

--- Tests/Engineer.test.js
+++ Tests/Engineer.test.js
@@ -1,3 +1,19 @@
 const Employee = require('../Lib/Employee');
 const Engineer = require('../Lib/Engineer.js');
 
+describe("Engineer", () => {
+    describe("Initialization", () => {
+        it("Should create a new object with Name, Id, Email, and Github when called with the 'new' keyword", () => {
+            const obj = new Employee("Andrew", 1111, "x@x.com", "Andrew11");
+            function getRole() {
+                return "Employee";
+            };
+
+            expect(Engineer.name !== null).toEqual(true);
+            expect(Engineer.id !== null).toEqual(true);
+            expect(Engineer.email !== null).toEqual(true);
+            expect(Engineer.github !== null).toEqual(true);
+            expect(getRole()).toEqual("Employee");
+        });
+    });
+});
